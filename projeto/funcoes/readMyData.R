1d9fa963f45358c840a722afb781cdeee91e116d
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-02 17:26:19 -0200

    funcao readMyData terminada, atualizado maincode e gitignore

--- projeto/funcoes/readMyData.R
+++ projeto/funcoes/readMyData.R
@@ -1,9 +1,16 @@
 readMyData <- function(gse){
-    for (i in seq_along(gse){
-
-	files = list.files(pattern=gse[i])
-	    
+    tally <- list("null")
+    for (i in seq_along(gse)){
+	x <- 69 # change this 
+	files <- list.files(pattern = gse[i])
+	system(paste0("gunzip ",files)) # esse comando só funciona no linux muahahaha
+	files <- list.files(pattern = gse[i])
+        data <- read.table(file = files, header = T, skip = x, fill = T)
+	assign(gse[i], data)
+	tally[[i]] <- data
     }	
-	
-	return(#lista de objetos com os dados lidos)
+    name <- ls(pattern = "GSE")
+    names(tally) <- name # renomeia lista
+    
+    return(tally)
 }
