aee1f045f826ad1f51b29bfb4b36f46b8d6961b0
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-04 09:30:26 -0200

    criacao do arquivo e esqueleto da funcao doColourPallete.R

---
+++ projeto/funcoes/doColourPallete.R
@@ -0,0 +1,13 @@
+# Função doColourPallete
+doColourPallete <- function(df, col="black", num=0){
+    if (col != "black" && num == 0){
+    
+    }
+    else if (col == "black" && num != 0){
+    
+    }
+    else {
+    
+    }
+    return(df)
+}
