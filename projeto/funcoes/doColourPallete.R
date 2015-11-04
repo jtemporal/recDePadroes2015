ef7093ca75eff73f7ea64925cab871cc2674606c
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-04 10:01:52 -0200

    andamento na funcao doColourPallete

--- projeto/funcoes/doColourPallete.R
+++ projeto/funcoes/doColourPallete.R
@@ -1,13 +1,31 @@
 # Função doColourPallete
-doColourPallete <- function(df, col="black", num=0){
-    if (col != "black" && num == 0){
-    
-    }
-    else if (col == "black" && num != 0){
-    
+doColourPallete <- function(df, type = "none", col = "black", num = 0){
+    if (type == "none"){
+        return("Você esqueceu de informar a categoria, corrija o codigo e rode novamente :)")
     }
     else {
-    
+        if (col != "black" && num == 0){
+            df$col = "black"
+            for (i in 1:length(type)) {
+                df$col[grep(type[i], df$patient)] = col[i]
+            }
+            return(df)
+        }
+        else if (col == "black" && num != 0){
+            df$col = col
+            col = sample(colours(),num)
+            for (i in 1:length(type)) {
+                df$col[grep(type[i], df$patient)] = col[i]
+            }
+            return(df)
+        }
+        else {
+            
+            
+            
+        }
+        return(df)
     }
-    return(df)
 }
+
+
