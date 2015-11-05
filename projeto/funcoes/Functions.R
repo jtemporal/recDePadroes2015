e650c6eb8303b025e078315282e56f53678d7ea7
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-05 21:31:04 -0200

    added doColourPalette to Functions.R

--- projeto/funcoes/Functions.R
+++ projeto/funcoes/Functions.R
@@ -79,3 +79,35 @@ doMeta <- function(gse){
     names(l) <- gse
     return(l)
 }
+
+# Função doColourPalette
+doColourPalette <- function(df, type = "none", col = "black", num = 0){
+    if (type == "none"){
+        return("Você esqueceu de informar a categoria, corrija o codigo e rode novamente :)")
+    }
+    else {
+        if (col != "black" && num == 0){
+            df$col = "black"
+            for (i in 1:length(type)) {
+                df$col[grep(type[i], df$patient)] = col[i]
+            }
+            return(df)
+        }
+        else if (col == "black" && num != 0){
+            df$col = col
+            col = sample(colours(),num)
+            for (i in 1:length(type)) {
+                df$col[grep(type[i], df$patient)] = col[i]
+            }
+            return(df)
+        }
+        else {
+            df$col = col
+            col = sample(colours(),length(type))
+            for (i in 1:length(type)) {
+                df$col[grep(type[i], df$patient)] = col[i]
+            }
+        }
+        return(df)
+    }
+}
