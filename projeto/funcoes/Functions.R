0645ec5d5795da4bc5faeffddd34303dbc1467f0
Jessica Temporal <jtemporal@users.noreply.github.com>
2015-11-05 20:52:51 -0200

    Update Functions.R

--- projeto/funcoes/Functions.R
+++ projeto/funcoes/Functions.R
@@ -53,61 +53,6 @@ readMyData <- function(gse){
 }
 
 # Função doMeta
-
-doMeta <- function(gse){
-    l <- list("null")
-    for (j in seq_along(gse)){
-        x <- readLines(con = paste0("http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=", gse[j]))
-        for (i in seq_along(x)){
-            gsmIndex <- grep("GSM", x)
-            gsm <- strsplit(x=x[gsmIndex], split='">')
-
-            patientIndex <- gsmIndex + 1
-            patient <- strsplit(x=x[patientIndex], split='">')
-
-            for (k in seq_along(gsmIndex)){
-                patient[[k]] <- patient[[k]][2]
-                patient[[k]] <- substr(patient[[k]], start=1, stop=nchar(patient[[k]])-5)
-
-                gsm[[k]] <- substr(gsm[[k]][length(gsm[[k]])], start=1, stop=10)
-            }
-        }
-    gsm <- unlist(gsm)
-    patient <- unlist(patient)
-    l[[j]] = cbind(gsm,patient)
-    }
-    names(l) <- gse
-    return(l)
-}
-# Função doMeta
-
-doMeta <- function(gse){
-    l <- list("null")
-    for (j in seq_along(gse)){
-        x <- readLines(con = paste0("http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=", gse[j]))
-        for (i in seq_along(x)){
-            gsmIndex <- grep("GSM", x)
-            gsm <- strsplit(x=x[gsmIndex], split='">')
-
-            patientIndex <- gsmIndex + 1
-            patient <- strsplit(x=x[patientIndex], split='">')
-
-            for (k in seq_along(gsmIndex)){
-                patient[[k]] <- patient[[k]][2]
-                patient[[k]] <- substr(patient[[k]], start=1, stop=nchar(patient[[k]])-5)
-
-                gsm[[k]] <- substr(gsm[[k]][length(gsm[[k]])], start=1, stop=10)
-            }
-        }
-    gsm <- unlist(gsm)
-    patient <- unlist(patient)
-    l[[j]] = cbind(gsm,patient)
-    }
-    names(l) <- gse
-    return(l)
-}
-
-# Função doMeta
 doMeta <- function(gse){
     l <- list("null")
     for (j in seq_along(gse)){
