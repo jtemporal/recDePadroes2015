ac8444e1c943d22f09bcd9ca97d4bceae1525f65
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-09 11:32:38 -0200

    ultimos upadates

--- projeto/funcoes/doColourPalette.R
+++ projeto/funcoes/doColourPalette.R
@@ -1,19 +1,19 @@
 # Função doColourPalette
-doColourPalette <- function(df, type = "none", col = "black", num = 0){
+doColourPalette <- function(df, type = "none", col = "black"){
     if (type == "none"){
         return("Você esqueceu de informar a categoria, corrija o codigo e rode novamente :)")
     }
     else {
-        if (col != "black" && num == 0){
+        if (col != "black"){
             df$col = "black"
             for (i in 1:length(type)) {
                 df$col[grep(type[i], df[,2])] = col[i]
             }
             return(df)
         }
-        else if (col == "black" && num != 0){
+        else if (col == "black"){
             df$col = col
-            col = sample(colours(),num)
+            col = sample(colours(),length(type))
             for (i in 1:length(type)) {
                 df$col[grep(type[i], df[,2])] = col[i]
             }
