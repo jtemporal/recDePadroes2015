1f5e994e16c0a58b8c4fa87059f000aeaaafc301
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-10 08:34:48 -0200

    corrigida função de cores e alguns comentários

--- projeto/funcoes/doColourPalette.R
+++ projeto/funcoes/doColourPalette.R
@@ -1,8 +1,9 @@
 # Função doColourPalette
+# esta função cria cores para os plots baseado nas categorias dos dados
+# passados pelo usuário
 # recebe df, type e col como parametro
-# df eh uma matriz, type e col sao string
-# a funcao adiciona cores para diferenciar os tipos de enfermidades 
-# retorna df
+# df eh um data.frame, type e col sao vetores de string 
+# retorna df com uma nova coluna (col) que contem as cores
 doColourPalette <- function(df, type = "none", col = "black"){
     # se type não possuir tipos de enfermidades 
     if (type == "none"){
@@ -10,7 +11,7 @@ doColourPalette <- function(df, type = "none", col = "black"){
     }
     else {
         # se col for igual a black, significa que  foram passado cores a col 
-        if (col != "black"){
+        if (col != "black" && length(col) == length(type)){
             # cria uma nova coluna col em df e adiciona a cor "black"
             df$col = "black"
             for (i in 1:length(type)) {
@@ -19,27 +20,22 @@ doColourPalette <- function(df, type = "none", col = "black"){
             }
             return(df)
         }
-        # se col for igual a black, significa que não foram passado cores a col 
-        else if (col == "black"){
-            # cria uma nova coluna col em df e adiciona a cor "black"
-            df$col = col
-            # col recebe cores aleatorias
-            col = sample(colours(),length(type))
-            for (i in 1:length(type)) {
-                # procura o tipo indicado em type[i] na coluna 2 de df e adiciona a cor de col[i] na coluna col de df
-                df$col[grep(type[i], df[,2])] = col[i]
-            }
-            return(df)
+        # se a quantidade de categorias não for a mesma que a quantidade de cores
+        else if (col != "black" && length(col) != length(type)){
+            return("A quantidade de cores não condiz com a quantidade de categorias\nCorrija um dos vetores e rode novamente! :)")
         }
-        else {
-            df$col = col 
-            col = sample(colours(),length(type))
-            for (i in 1:length(type)) {
-                df$col[grep(type[i], df[,2])] = col[i]
-            }
+        # se col for igual a black, significa que não foram passado cores pelo usuário
+        else (col == "black"){
+                # cria uma nova coluna col em df e preenche com  a cor "black"
+                df$col = col
+                # col recebe cores aleatorias
+                col = sample(colours(),length(type))
+                for (i in 1:length(type)) {
+                    # procura o tipo indicado em type[i] na coluna 2 de df e adiciona a cor de col[i] na coluna col de df
+                    df$col[grep(type[i], df[,2])] = col[i]
+                }
+                return(df)
+            } 
         }
-        return(df)
     }
-}
-
 
