6dbc921ab5bb41fa252feb6b6454af89a3c81565
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-24 19:25:57 -0200

    terminado comentarios em doMeta

--- funcoes/doMeta.R
+++ funcoes/doMeta.R
@@ -11,23 +11,22 @@ doMeta <- function(gse){
         x <- readLines(con = paste0("http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=", gse[j]))
         # anota as linhas que possuem o termo GSM da pagina 
         gsmIndex <- grep("GSM", x)
+        # faz as manipulacoes de string para obter os gsm de cada amostra
         gsm <- strsplit(x=x[gsmIndex], split='">')
-
-        patientIndex <- gsmIndex + 1
-        patient <- strsplit(x=x[patientIndex], split='">')
-
-        
-	    for (k in seq_along(gsmIndex)){
-                patient[[k]] <- patient[[k]][2]
-                patient[[k]] <- substr(patient[[k]], start=1, stop=nchar(patient[[k]])-5)
-                
-                gsm[[k]] <- substr(gsm[[k]][length(gsm[[k]])], start=1, stop=10)
-            }
-        
-    gsm <- unlist(gsm)
-    patient <- unlist(patient)
-    l[[j]] <- cbind(gsm,patient)
+        # anota o tipo de paciente de quem amostra foi colhida
+        patient <- strsplit(x=x[gsmIndex + 1], split='">')
+        # manipulacao de listas e strings para manter apenas as informações necessarias
+        for (k in seq_along(gsmIndex)){
+            patient[[k]] <- patient[[k]][2]
+            patient[[k]] <- substr(patient[[k]], start=1, stop=nchar(patient[[k]])-5)
+            gsm[[k]] <- substr(gsm[[k]][length(gsm[[k]])], start=1, stop=10)
+        }
+        # tornado gsm e patient em vetores p/ serem adicionaddos na lista final
+        gsm <- unlist(gsm)
+        patient <- unlist(patient)
+        l[[j]] <- cbind(gsm,patient)
     }
+    # renomeando os niveis da lista para corresponder ao estudo
     names(l) <- gse 
     return(l)
 }
