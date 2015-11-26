17dd275c39e7f41c16cf0700184b32557f50dc0d
raissapoch <raissa.poch@gmail.com>
2015-11-25 23:22:28 -0200

    Update Functions.R
    Atualizei a função doMeta

--- funcoes/Functions.R
+++ funcoes/Functions.R
@@ -58,25 +58,19 @@ doMeta <- function(gse){
     l <- list("null")
     for (j in seq_along(gse)){
         x <- readLines(con = paste0("http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=", gse[j]))
-        for (i in seq_along(x)){
-            gsmIndex <- grep("GSM", x)
-            gsm <- strsplit(x=x[gsmIndex], split='">')
-
-            patientIndex <- gsmIndex + 1
-            patient <- strsplit(x=x[patientIndex], split='">')
-
-            for (k in seq_along(gsmIndex)){
-                patient[[k]] <- patient[[k]][2]
-                patient[[k]] <- substr(patient[[k]], start=1, stop=nchar(patient[[k]])-5)
-
-                gsm[[k]] <- substr(gsm[[k]][length(gsm[[k]])], start=1, stop=10)
-            }
+        gsmIndex <- grep("GSM", x)
+        gsm <- strsplit(x=x[gsmIndex], split='">')
+        patient <- strsplit(x=x[gsmIndex + 1], split='">')
+        for (k in seq_along(gsmIndex)){
+            patient[[k]] <- patient[[k]][2]
+            patient[[k]] <- substr(patient[[k]], start=1, stop=nchar(patient[[k]])-5)
+            gsm[[k]] <- substr(gsm[[k]][length(gsm[[k]])], start=1, stop=10)
         }
-    gsm <- unlist(gsm)
-    patient <- unlist(patient)
-    l[[j]] = cbind(gsm,patient)
+        gsm <- unlist(gsm)
+        patient <- unlist(patient)
+        l[[j]] <- cbind(gsm,patient)
     }
-    names(l) <- gse
+    names(l) <- gse 
     return(l)
 }
 
